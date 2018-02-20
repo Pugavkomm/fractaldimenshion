c2e5670cc7230ca78f2b3ecc3e15cf28e2a3fcb6
mechislav <slavapugavko2@gmail.com>
2018-02-20 21:37:58 +0300

    cut

--- apr.py
+++ apr.py
@@ -1,7 +1,7 @@
 import scipy as sp
 import matplotlib.pyplot as plt
 def mnkGP(x,y):
-    d=1 # степень полинома
+    d=2 # степень полинома
     fp, residuals, rank, sv, rcond = sp.polyfit(x, y, d, full=True) # Модель
     f = sp.poly1d(fp) # аппроксимирующая функция
 
